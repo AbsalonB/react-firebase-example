d906df662ddc7fbbd536c3761a08fe74a6458ab8
Absalon Blanco <absalon.blanco@hotmail.com>
2023-08-12 12:39:56 -0600

    Implementation of Formik and Yup for form validations.

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { login } from "../config/firebase";
 import { Link, useNavigate } from "react-router-dom";
 import { useUserContext } from "../context/UserContext";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
 const Login = () => {
     const [email,setEmail] = useState("");
@@ -12,32 +14,75 @@ const Login = () => {
         if(user){
             navigate('/dashboard');
         }
-    },[user]);
+    },[user]); 
 
-    const handleSubmit = async(e) =>{
+    const onSubmit = async ({email,password},{setSubmitting, setErrors, resetForm}) =>{
         e.preventDefault();
         try {
            const credentialUser = await login({email,password});
            console.log(credentialUser);
             
-        } catch (error) {
-            console.log(error);
+        } catch (error) { 
+            if(error.code ==="auth/user-not-found"){
+                return setErrors({email:"Usuario no registrado"});
+            }
+            if(error.code =="auth/wrong-password"){
+                return setErrors({password:"Contrasena incorrecta"});
+            }
+        } finally{
+            setSubmitting(false); 
         }
     }
+
+    const validationSchema = Yup.object().shape({
+        email: Yup.string().email("Email no válido").required("Email requerido"),
+        password : Yup.string().min(6, "Mínimo 6 carácteres").required("Password requerido")
+    })
+
     return (
         <>
             <h1>Login</h1>
-            <form onSubmit={handleSubmit}>
-                <input type="text" 
-                placeholder="Ingrese email" 
-                value={email} 
-                onChange={(e)=>setEmail(e.target.value)} />
-                <input type="password" 
-                placeholder="Ingrese correo" 
-                value={password} 
-                onChange={(e)=>setPassowrd(e.target.value)} />
-                <button type="submit">Login</button>
-            </form>
+            <Formik
+                initialValues={{email:"", password:""}}
+                onSubmit={onSubmit}
+                validationSchema={validationSchema}
+            >
+                {
+                    ({values,
+                         handleSubmit, 
+                         handleChange, 
+                         errors, 
+                         touched, 
+                         handleBlur,
+                         isSubmitting,
+                        })=>(
+                        <form onSubmit={handleSubmit}>
+                            <input type="text" 
+                            placeholder="Ingrese email" 
+                            value={values.email} 
+                            onChange={handleChange} 
+                            name="email"
+                            onBlur={handleBlur}
+                            />
+                            {
+                                errors.email && touched.email && errors.email
+                            }
+                            <input type="password" 
+                            placeholder="Ingrese correo" 
+                            value={values.password} 
+                            onChange={handleChange} 
+                            name="password"
+                            onBlur={handleBlur}
+                            />
+                            {
+                                errors.password && touched.password && errors.password
+                            }
+                            <button type="submit" disabled={isSubmitting}>Login</button>
+                        </form>
+                    )
+                }
+            </Formik>
+           
             <Link to="register">Create user</Link>
         </>
     )
